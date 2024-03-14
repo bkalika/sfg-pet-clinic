163fcce35e898d50484c41acd28d10ede7965b12
Bohdan Kalika <bogdan.kalika@gmail.com>
2024-03-14 23:58:23 +0200

    Adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by bogdan.kalika@gmail.com
+ * Date: 3/14/2024
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by bogdan.kalika@gmail.com
+ * Date: 3/14/2024
+ */
+public class Vet extends Person {
+}
