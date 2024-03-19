9dfe8820a8ee0a9504969d9e685ea2b3098e1632
Bohdan Kalika <bogdan.kalika@gmail.com>
2024-03-19 22:13:45 +0200

    Closes #8.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by bogdan.kalika@gmail.com
+ * Date: 3/19/2024
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
