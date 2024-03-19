4096b415a3d804ddb2cfebc159ae6eee600aa2e3
Bohdan Kalika <bogdan.kalika@gmail.com>
2024-03-19 21:50:19 +0200

    Implement Map based Service. Closes #6.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,9 +1,11 @@
 package guru.springframework.sfgpetclinic.services;
 
+import guru.springframework.sfgpetclinic.model.Vet;
+
 /**
  * Created by bogdan.kalika@gmail.com
  * Date: 3/18/2024
  */
-public interface Vet extends CrudService<Vet, Long> {
+public interface VetService extends CrudService<Vet, Long> {
 
 }
